0aff6ae7c4a339bb46e14eb26eb88a623cfd0c1b
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-07-18 15:17:05 -0400

    added first component

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() { 
+        return ( 
+            <div>
+                Hello from Counter
+            </div>
+         );
+    }
+}
+ 
+export default Counter;
