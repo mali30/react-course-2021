2831be1721974b4976c05a540d5031919a3b699e
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-07-18 15:36:34 -0400

    added styles attributes

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -4,19 +4,33 @@ class Counter extends Component {
   // object that includes any data we need
   state = {
     count: 0,
+    // imageUrl: "https://picsum.photos/200",
+  };
+
+  styles = {
+    fontSize: 40,
+    fontWeight: "bold",
   };
   render() {
     return (
       <React.Fragment>
-        <span> {this.formatCount()} </span>
-        <button>Click Me</button>
+        <img src={this.state.imageUrl} alt=""></img>
+        {/* style object */}
+        <span style={this.styles} className="badge badge-primary m-2">
+          {" "}
+          {this.formatCount()}{" "}
+        </span>
+        {/* inline styles */}
+        <button style={{ fontSize: 30 }} className="btn btn-secondary btn-sm">
+          Increment
+        </button>
       </React.Fragment>
     );
   }
 
   formatCount = () => {
     const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    return count === 0 ? "Zero" : count;
   };
 }
 
