af90ad0fa512ed2589deea92c25944a7f2e7f279
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-07-18 15:28:41 -0400

    added expressions

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  // object that includes any data we need
+  state = {
+    count: 0,
+  };
   render() {
     return (
       <React.Fragment>
-        <h1>Hello from Counter </h1>
+        <span> {this.formatCount()} </span>
         <button>Click Me</button>
       </React.Fragment>
     );
   }
+
+  formatCount = () => {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  };
 }
 
 export default Counter;
