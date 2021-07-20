10fe2f425fa86c79907b46a57014f423f8c2b173
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-07-20 15:58:44 -0400

    passing props in counter component

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -4,16 +4,16 @@ import Counter from "../components/counter";
 class Counters extends Component {
   state = {
     counter: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
     ],
   };
   render() {
     return (
       <div>
         {this.state.counter.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
       </div>
     );
