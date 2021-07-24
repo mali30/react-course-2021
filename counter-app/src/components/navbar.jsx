cf7a427c1e4b58f4acebc74ce2497276a9b41e0c
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-07-24 13:04:55 -0400

    converted to a stateless functional componenet

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
-
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-                    Navbar <span className="badge badge-pilll badge-secondary">{this.props.totalCounters}</span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+          <span className="badge badge-pilll badge-secondary">
+            {props.totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
