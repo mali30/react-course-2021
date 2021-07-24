3553056170c642b00319eb9eaf6e7bb21f4c78dc
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-07-24 11:52:08 -0400

    parent component to counter. added new handle functions and onIncrement prop

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -3,45 +3,59 @@ import Counter from "../components/counter";
 
 class Counters extends Component {
   state = {
-    counter: [
+    counters: [
       { id: 1, value: 1 },
       { id: 2, value: 2 },
       { id: 3, value: 3 },
     ],
   };
 
-    handleReset = () => {
-        const counters = this.state.counter.map((c) => {
-            c.value = 0;
-            return c;
-        });
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
 
-        this.setState({
-            counter: counters
-        })
-    };
+    this.setState({
+      counters: counters,
+    });
+  };
 
   handleDelete = (counterId) => {
-    const counters = this.state.counter.filter(
-      (idToFilterOn) => counterId !== idToFilterOn.value
+    const counter = this.state.counters.filter(
+      (idToFilterOn) => counterId !== idToFilterOn.id
     );
     this.setState({
-      counter: counters,
+      counters: counter,
     });
     console.log("counterId", counterId);
   };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    // counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({
+      counters,
+    });
+  };
   render() {
     return (
-        <div>
-            <button onClick ={this.handleReset} className="btn btn-primary btn-sm m2">Reset</button>
-        {this.state.counter.map((counter) => (
+      <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m2"
+        >
+          Reset
+        </button>
+        {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
             counter={counter}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
           >
-            {/* passing a child element into our component */}
-            {/* <h3> The id of each counter {counter.id}</h3> */}
           </Counter>
         ))}
       </div>
