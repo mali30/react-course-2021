93e4b2e7a0f41405da7879120387c0cf8cb9edfc
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-07-24 20:15:07 -0400

    added like component for liking movies

---
+++ vidly/src/components/common/like.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+
+  return (
+    <div>
+      <i
+        onClick={props.onClick}
+        className={classes}
+        style={{ cursor: "pointer" }}
+        aria-hidden="true"
+      ></i>
+    </div>
+  );
+};
+
+export default Like;
