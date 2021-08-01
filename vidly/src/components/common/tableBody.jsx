226e34e9e9b16c96a3de7df4b3a8f938948e81af
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-08-01 12:22:51 -0400

    moved the movies table into a resuable component called tableBody

---
+++ vidly/src/components/common/tableBody.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import _ from "lodash";
+
+class TableBody extends Component {
+    renderCell = (item, column) => {
+      // since like and delete button have a field called content
+        // we want to return that content
+    if (column.content) return column.content(item);
+    
+      // in this case item represents each movie in the movie object
+      // from the getAllMovies service
+      // column.path represents each column we want to display from the movies object
+      return _.get(item, column.path);
+  };
+  render() {
+    const { data, columns } = this.props;
+    return (
+      <tbody>
+        {data.map((item) => (
+          <tr>
+                {columns.map((column) => (
+              <td>{this.renderCell(item, column)}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+}
+
+export default TableBody;
