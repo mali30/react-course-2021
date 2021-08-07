2fbd13822dae0825407cff0bc75cd7fca2bc9dc7
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-08-07 19:53:12 -0400

    added navbar component

--- vidly/src/App.jsx
+++ vidly/src/App.jsx
@@ -1,25 +1,29 @@
 import React, { Component } from "react";
-import "./App.css";
-import Movies from "./components/movies";
 import { Route, Redirect, Switch } from "react-router-dom";
+
+import Movies from "./components/movies";
 import NotFound from "./components/notFound";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
+import Navbar from "../src/components/common/navbar";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        {/* <Movies /> */}
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/"exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
