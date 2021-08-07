c8a5dbf7ccfaa7242eaa7c9bec7f56641a82725f
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-08-07 19:30:04 -0400

    added more routes and respective files

---
+++ vidly/src/components/loginForm.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class LogInForm extends Component {
+
+    render() { 
+        return (  
+            <h1>Login</h1>
+        );
+    }
+}
+ 
+export default LogInForm;
