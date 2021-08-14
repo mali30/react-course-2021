abc8ed13531a6f45bde7a9b97985672bc4a82f94
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-08-14 19:01:58 -0400

    added initial login form

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,12 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class LogInForm extends Component {
+  render() {
+    const handleSubmit = (event) => {
+      // prevents default behavrior
+      event.preventDefault();
 
-    render() { 
-        return (  
-            <h1>Login</h1>
-        );
-    }
+      console.log("Submited");
+    };
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">UserName</label>
+            <input autoFocus id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Log In</button>
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default LogInForm;
