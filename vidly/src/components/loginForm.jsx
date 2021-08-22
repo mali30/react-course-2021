128cf877e1307dc056b8899cdef08fc57caf1782
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-08-22 12:49:05 -0400

    moved input component into resuable form component

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Input from "./common/input";
 import Joi from "joi-browser";
 import Form from "./common/form";
 
@@ -22,29 +21,13 @@ class LogInForm extends Form {
   };
 
   render() {
-    const { data, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.username}
-            label="Username"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-
-          <Input
-            name="password"
-            value={data.password}
-            label="Password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button disabled={this.validate()} className="btn btn-primary">
-            Log In
-          </button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
