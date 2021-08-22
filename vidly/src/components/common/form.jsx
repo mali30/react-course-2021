c4075e57d45168df95b8910975394f4dbb64bb36
Mohamed Ali <mohamed.ali8434@gmail.com>
2021-08-22 18:14:03 -0400

    added function for input component to display movie genres on movie form

--- vidly/src/components/common/form.jsx
+++ vidly/src/components/common/form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./input";
+import Select from "./select";
 
 class Form extends Component {
   state = {
@@ -55,12 +56,16 @@ class Form extends Component {
     });
     if (errors) return;
 
-    this.doSubmit();
+    this.doSubmit(event);
   };
 
   renderButton(label) {
     return (
-      <button disabled={this.validate()} className="btn btn-primary">
+      <button
+        onClick={this.handleSubmit}
+        disabled={this.validate()}
+        className="btn btn-primary"
+      >
         {label}
       </button>
     );
@@ -77,6 +82,21 @@ class Form extends Component {
         label={label}
         onChange={this.handleChange}
         error={errors[name]}
+      />
+    );
+  }
+
+  renderSelect(name, label, options) {
+    const { data, errors } = this.state;
+
+    return (
+      <Select
+        name={name}
+        value={data[name]}
+        label={label}
+        options={options}
+        onChange={this.handleChange}
+        errors={errors[name]}
       />
     );
   }
